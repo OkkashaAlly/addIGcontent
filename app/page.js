c142371ffb3e26c42f935cfecc3ccca27853d78e
OkkashaAlly <steamnature006@gmail.com>
2023-04-23 15:26:44 +0300

    fixed environment vars

--- app/page.js
+++ app/page.js
@@ -5,24 +5,20 @@ import { useSearchParams } from "next/navigation";
 
 export default function Home() {
   const searchParams = useSearchParams();
-
   const code = searchParams.get("code");
 
-  console.log("code: ", code);
-  console.log(
-    "process.env.INSTAGRAM_CLIENT_ID: ",
-    process.env.INSTAGRAM_CLIENT_ID
-  );
-  console.log(
-    "process.env.INSTAGRAM_CLIENT_SECRET: ",
-    process.env.INSTAGRAM_CLIENT_SECRET
-  );
+  const INSTAGRAM_CLIENT_ID = "1316832365714113";
+  const INSTAGRAM_CLIENT_SECRET = "024150d52dbdb4ef7c9ad9dcf10e2c6c";
 
-  const url = `https://api.instagram.com/oauth/authorize?client_id=${process.env.INSTAGRAM_CLIENT_ID}&redirect_uri=https://add-ig-content.vercel.app/&scope=user_profile,user_media&response_type=code`;
+  console.log("code: ", code);
+  console.log("INSTAGRAM_CLIENT_ID: ", INSTAGRAM_CLIENT_ID);
+  console.log("INSTAGRAM_CLIENT_SECRET: ", INSTAGRAM_CLIENT_SECRET);
+
+  const url = `https://api.instagram.com/oauth/authorize?client_id=${INSTAGRAM_CLIENT_ID}&redirect_uri=https://add-ig-content.vercel.app/&scope=user_profile,user_media&response_type=code`;
 
   const getToken = async () => {
     const response = await axios.post(
-      `https://api.instagram.com/oauth/access_token?client_id=${process.env.INSTAGRAM_CLIENT_ID}&client_secret=${process.env.INSTAGRAM_CLIENT_SECRET}&grant_type=authorization_code&redirect_uri=https://add-ig-content.vercel.app/&code=${code}`,
+      `https://api.instagram.com/oauth/access_token?client_id=${INSTAGRAM_CLIENT_ID}&client_secret=${INSTAGRAM_CLIENT_SECRET}&grant_type=authorization_code&redirect_uri=https://add-ig-content.vercel.app/&code=${code}`,
       {
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
