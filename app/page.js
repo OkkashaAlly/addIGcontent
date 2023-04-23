1286be410c3b39c2fae80cb8a5e9d37e533db3f1
OkkashaAlly <steamnature006@gmail.com>
2023-04-23 14:37:13 +0300

    getting url param data

--- app/page.js
+++ app/page.js
@@ -1,19 +1,34 @@
 "use client";
 
 import axios from "axios";
+import { useSearchParams } from 'next/navigation';
 
 export default function Home() {
-  const handleSingIn = async () => {
-    const url = `https://api.instagram.com/oauth/authorize?client_id=${process.env.INSTAGRAM_CLIENT_ID}&redirect_uri=https://add-ig-content.vercel.app/&scope=user_profile,user_media&response_type=code`;
+  const searchParams = useSearchParams();
 
-    const response = await axios.get(url, { withCredentials: true });
+  const code = searchParams.get('code');
 
-    console.log("Response: ", response);
-  };
+  console.log("code: ", code);
+  
+  const url = `https://api.instagram.com/oauth/authorize?client_id=${process.env.INSTAGRAM_CLIENT_ID}&redirect_uri=https://add-ig-content.vercel.app/&scope=user_profile,user_media&response_type=code`;
+
+
+
+  // const handleSingIn = async () => {
+  //   console.log(
+  //     "process.env.INSTAGRAM_CLIENT_ID: ",
+  //     process.env.INSTAGRAM_CLIENT_ID
+  //   );
+
+  //   const response = await axios.get(url, { withCredentials: true });
+
+  //   console.log("Response: ", response);
+  // };
 
   return (
     <main>
-      <button onClick={handleSingIn}>Sign In instagram</button>
+      <h1>COde: {code}</h1>
+      <a href={url}>Sign In instagram</a>
     </main>
   );
 }
