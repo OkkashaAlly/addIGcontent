800c725211443bf74234381f3e0668fc7eb2b48f
OkkashaAlly <steamnature006@gmail.com>
2023-04-23 14:06:57 +0300

    cors fix

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   const handleSingIn = async () => {
     const url = `https://api.instagram.com/oauth/authorize?client_id=${process.env.INSTAGRAM_CLIENT_ID}&redirect_uri=https://add-ig-content.vercel.app/&scope=user_profile,user_media&response_type=code`;
 
-    const response = await axios.get(url);
+    const response = await axios.get(url, { withCredentials: true });
 
     console.log("Response: ", response);
   };
