0d07fa25760c5799bcf5bd6a299574749340a167
OkkashaAlly <steamnature006@gmail.com>
2023-04-23 15:12:07 +0300

    getting token 2

--- app/page.js
+++ app/page.js
@@ -1,19 +1,17 @@
 "use client";
 
-// import axios from "axios";
-import { useSearchParams } from 'next/navigation';
+import axios from "axios";
+import { useSearchParams } from "next/navigation";
 
 export default function Home() {
   const searchParams = useSearchParams();
 
-  const code = searchParams.get('code');
+  const code = searchParams.get("code");
 
-  console.log("code: ", code);
-  
   const url = `https://api.instagram.com/oauth/authorize?client_id=${process.env.INSTAGRAM_CLIENT_ID}&redirect_uri=https://add-ig-content.vercel.app/&scope=user_profile,user_media&response_type=code`;
 
   const getToken = async () => {
-    const response = await fetch(
+    const response = await axios.post(
       `https://api.instagram.com/oauth/access_token?client_id=${process.env.INSTAGRAM_CLIENT_ID}&client_secret=${process.env.INSTAGRAM_CLIENT_SECRET}&grant_type=authorization_code&redirect_uri=https://add-ig-content.vercel.app/&code=${code}`
     );
 
@@ -22,27 +20,13 @@ export default function Home() {
     console.log("data: ", data);
   };
 
-  if (code) {
-    getToken()
-  }
-
-
-
-  // const handleSingIn = async () => {
-  //   console.log(
-  //     "process.env.INSTAGRAM_CLIENT_ID: ",
-  //     process.env.INSTAGRAM_CLIENT_ID
-  //   );
-
-  //   const response = await axios.get(url, { withCredentials: true });
-
-  //   console.log("Response: ", response);
-  // };
-
   return (
     <main>
-      <h1>COde: {code}</h1>
-      <a href={url}>Sign In instagram</a>
+      {!code ? (
+        <a href={url}>Sign In instagram</a>
+      ) : (
+        <button onClick={getToken}>Get token</button>
+      )}
     </main>
   );
 }
