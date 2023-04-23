d4efc35f2b6bd6e0758999868f38a522eaacb0b8
OkkashaAlly <steamnature006@gmail.com>
2023-04-23 15:21:30 +0300

    log data

--- app/page.js
+++ app/page.js
@@ -8,11 +8,26 @@ export default function Home() {
 
   const code = searchParams.get("code");
 
+  console.log("code: ", code);
+  console.log(
+    "process.env.INSTAGRAM_CLIENT_ID: ",
+    process.env.INSTAGRAM_CLIENT_ID
+  );
+  console.log(
+    "process.env.INSTAGRAM_CLIENT_SECRET: ",
+    process.env.INSTAGRAM_CLIENT_SECRET
+  );
+
   const url = `https://api.instagram.com/oauth/authorize?client_id=${process.env.INSTAGRAM_CLIENT_ID}&redirect_uri=https://add-ig-content.vercel.app/&scope=user_profile,user_media&response_type=code`;
 
   const getToken = async () => {
     const response = await axios.post(
-      `https://api.instagram.com/oauth/access_token?client_id=${process.env.INSTAGRAM_CLIENT_ID}&client_secret=${process.env.INSTAGRAM_CLIENT_SECRET}&grant_type=authorization_code&redirect_uri=https://add-ig-content.vercel.app/&code=${code}`
+      `https://api.instagram.com/oauth/access_token?client_id=${process.env.INSTAGRAM_CLIENT_ID}&client_secret=${process.env.INSTAGRAM_CLIENT_SECRET}&grant_type=authorization_code&redirect_uri=https://add-ig-content.vercel.app/&code=${code}`,
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
     );
 
     const data = await response.json();
